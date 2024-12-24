7aecd0e8627efd28f7a8e56200b74f2f8ad28cd8
chacha86 <taejincha0619@gmail.com>
2024-12-24 10:38:01 +0900

    work12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,10 @@ class App {
     public void run() {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("== 명언 앱 ==");
         int lastNo = 0;
-        String[] contentList = new String[3];
-        String[] authorList = new String[3];
+        WiseSaying[] wiseSayingList = new WiseSaying[3];
 
-        WiseSaying wiseSaying = new WiseSaying();
-
+        System.out.println("== 명언 앱 ==");
         while(true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
@@ -35,18 +32,24 @@ class App {
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
+                WiseSaying wiseSaying = new WiseSaying();
 
                 wiseSaying.id = ++lastNo;
                 wiseSaying.content = content;
                 wiseSaying.author = author;
+
+                wiseSayingList[lastNo - 1] = wiseSaying;
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo));
             } else if(command.equals("목록")) {
 
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
 
+                for(int i = 0; i < lastNo; i++) {
+                    WiseSaying wiseSaying = wiseSayingList[i];
+                    System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+                }
 
 //                for(int i = 2; i >= 0; i--) {
 //                    System.out.println("%d / %s / %s".formatted(i, authorList[i], authorList[i]));
