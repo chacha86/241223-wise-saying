bf37cb40663beac8c08fce446d9b8ab2f2497640
chacha86 <taejincha0619@gmail.com>
2024-12-24 09:30:39 +0900

    work8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,8 @@ class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         int lastNo = 0;
+        String content = "";
+        String author = "";
 
         while(true) {
             System.out.print("명령) ");
@@ -21,15 +23,21 @@ class App {
             if(command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
+
             } else if(command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                content = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
-                scanner.nextLine();
+                author = scanner.nextLine();
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastNo));
+            } else if(command.equals("목록")) {
+
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println("%d / %s / %s".formatted(lastNo, author, content));
             }
         }
     }
