1299beda41db9f62b6d040d4039f11aa442bdc25
chacha86 <taejincha0619@gmail.com>
2025-01-02 09:51:21 +0900

    work31

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,22 @@
+import wiseSaying.WiseSayingController;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    private int lastId = 0;
-    private final ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
 
-    Scanner scanner = new Scanner(System.in);
+    private final WiseSayingController wiseSayingController;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public App() {
+        wiseSayingController = new WiseSayingController();
+    }
+
 
     public void run() {
 
-        add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
-        add("현재를 사랑하라", "작자 미상");
+        wiseSayingController.add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
+        wiseSayingController.add("현재를 사랑하라", "작자 미상");
 
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -22,91 +28,21 @@ public class App {
                 break;
 
             } else if (command.equals("등록")) {
-                writeWiseSaying();
+                wiseSayingController.writeWiseSaying();
             } else if (command.equals("목록")) {
-                printWiseSayingList();
+                wiseSayingController.printWiseSayingList();
             } else if (command.startsWith("삭제?id=")) {
                 String strId = command.substring(6);
                 int id = Integer.parseInt(strId);
-                deleteWiseSaying(id);
+                wiseSayingController.deleteWiseSaying(id);
             } else if (command.startsWith("수정?id=")) {
                 String strId = command.substring(6);
                 int id = Integer.parseInt(strId);
-                updateWiseSaying(id);
+                wiseSayingController.updateWiseSaying(id);
             }
         }
     }
 
-    private WiseSaying findWiseSaying(int targetId) {
 
-        for (WiseSaying wiseSaying : wiseSayingList) {
-            if (wiseSaying.getId() == targetId) {
-                return wiseSaying;
-            }
-        }
-
-        return null; // 자바에서 null은 객체가 없음 의미
-    }
-
-    private void updateWiseSaying(int targetId) {
-
-        WiseSaying wiseSaying = findWiseSaying(targetId);
-
-        if (wiseSaying == null) {
-            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(targetId));
-            return;
-        }
-
-        System.out.println("명언(기존) : %s".formatted(wiseSaying.getContent()));
-        System.out.print("명언 : ");
-        String newContent = scanner.nextLine();
-        System.out.println("명언(작가) : %s".formatted(wiseSaying.getAuthor()));
-        System.out.print("작가 : ");
-        String newAuthor = scanner.nextLine();
-
-        wiseSaying.setContent(newContent);
-        wiseSaying.setAuthor(newAuthor);
-
-        System.out.println("%d번 명언이 수정되었습니다.".formatted(targetId));
-
-    }
-
-    private void deleteWiseSaying(int targetId) {
-
-        WiseSaying wiseSaying = findWiseSaying(targetId);
-
-        if (wiseSaying == null) {
-            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(targetId));
-            return;
-        }
-
-        wiseSayingList.remove(wiseSaying);
-        System.out.println("%d번 명언이 삭제되었습니다.".formatted(targetId));
-    }
-
-    private void printWiseSayingList() {
-        System.out.println("번호 / 작가 / 명언");
-        System.out.println("----------------------");
-
-        for (WiseSaying wiseSaying : wiseSayingList.reversed()) {
-            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
-        }
-    }
-
-    private void writeWiseSaying() {
-        System.out.print("명언 : ");
-        String content = scanner.nextLine();
-
-        System.out.print("작가 : ");
-        String author = scanner.nextLine();
-
-        add(content, author);
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
-    }
-
-    public void add(String content, String author) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayingList.add(wiseSaying);
-    }
 
 }
